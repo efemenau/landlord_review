4fdc2f09fe7e47e2385d11c3015becd831811f25
Efemena <Menasglamzz@yahoo.com>
2025-04-04 12:52:01 +0300

    Add footer.php for website footer layout and contact information

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h5>TenantReview</h5>
+                <p class="small">Transparent rental experiences since 2023</p>
+            </div>
+            <div class="col-md-3">
+                <h5>Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="about.php" class="text-white text-decoration-none">About</a></li>
+                    <li><a href="contact.php" class="text-white text-decoration-none">Contact</a></li>
+                    <li><a href="privacy.php" class="text-white text-decoration-none">Privacy</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Contact</h5>
+                <ul class="list-unstyled">
+                    <li class="text-white">support@tenantreview.com</li>
+                    <li class="text-white">+1 (555) 123-4567</li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5>Follow Us</h5>
+                <div class="d-flex gap-3">
+                    <a href="#" class="text-white"><i class="fa-brands fa-x-twitter"></i></a>
+                    <a href="#" class="text-white"><i class="fa fa-facebook"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <hr class="mt-5">
+        <p class="text-center small">&copy; <?php echo date('Y'); ?> TenantReview. All rights reserved.</p>
+    </div>
+</footer>
+
+<!-- Bootstrap JS and Popper.js -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Custom JavaScript -->
+<script src="assets/js/main.js"></script> 
+</body>
+</html>
